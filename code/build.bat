725f933f0576efc980a7d350465a203b63683ae7
rivten <rivten.grey@gmail.com>
2017-09-25 09:51:52 +0200

    add batch file for win32 compilation

---
+++ code/build.bat
@@ -0,0 +1,22 @@
+@echo off
+
+rem --------------------------------------------------------------------------
+rem                        COMPILATION
+rem --------------------------------------------------------------------------
+
+set SDLPath=C:\SDL2-2.0.5\
+set SDLBinPath=%SDLPath%\lib\x64\
+
+set STBPath=..\..\stb\
+set RivtenPath=..\..\rivten\
+
+set UntreatedWarnings=/wd4100 /wd4244 /wd4201 /wd4127 /wd4505 /wd4456 /wd4996 /wd4003
+set CommonCompilerDebugFlags=/MT /Od /Oi /fp:fast /fp:except- /Zo /Gm- /GR- /EHa /WX /W4 %UntreatedWarnings% /Z7 /nologo /I %SDLPath%\include\ /I %STBPath% /I %RivtenPath%
+set CommonLinkerDebugFlags=/incremental:no /opt:ref /subsystem:console %SDLBinPath%\SDL2.lib %SDLBinPath%\SDL2main.lib /ignore:4099
+
+pushd ..\build\
+cl %CommonCompilerDebugFlags% ..\code\sdl_synchess.cpp /link %CommonLinkerDebugFlags%
+popd
+
+rem --------------------------------------------------------------------------
+echo Compilation completed...
